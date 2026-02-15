3aec4975bfa592eb43280a34d10142d0e4bb943f
Praveen Kadam <93198500+Praveenvkadam@users.noreply.github.com>
2026-02-15 20:00:27 +0530

    updt

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT;
 const startServer = async () => {
   try {
     await initDB();
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
