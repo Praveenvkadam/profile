46e6c0c56aaf78d794a02992c8e72878e3dfc855
pkadam7 <praveenkadam7411@gmail.com>
2026-02-16 00:36:22 +0530

    r

--- frontend/app/(auth)/forgetpassword/page.jsx
+++ frontend/app/(auth)/forgetpassword/page.jsx
@@ -14,6 +14,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { resetPassword } from "../../api/auth_api";
 
+//.
 const schema = z
   .object({
     email: z.string().min(1, "Email is required").email("Invalid email"),
